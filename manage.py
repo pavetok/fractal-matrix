327b3f94656606398309dcb96899f3afe98fae6d
pavetok <p.vetokhin@gmail.com>
2014-05-20 22:12:57 +0400

    added db migration

---
+++ manage.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+import os
+from app import create_app, db
+from app.models import Aspect, Universum
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, Aspect=Aspect, Universum=Universum)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
