768a89703c6909b945386e5bdff507e5e73f2e4a
pavetok <p.vetokhin@gmail.com>
2014-06-04 23:25:15 +0400

    moved static folder

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -4,6 +4,8 @@ from flask import Blueprint
 
 core = Blueprint('core',
                  __name__,
-                 template_folder='templates')
+                 template_folder='templates',
+                 static_folder='static',
+                 static_url_path='/core/static')
 
 from . import views
---
+++ migrations/versions/1782fa3768c_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 1782fa3768c
+Revises: 34ee732dfe
+Create Date: 2014-06-01 18:30:02.186572
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1782fa3768c'
+down_revision = '34ee732dfe'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
