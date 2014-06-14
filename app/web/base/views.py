50c46b440d5f0d05f65e40d04759b2a900688290
pavetok <p.vetokhin@gmail.com>
2014-06-14 10:42:22 +0400

    added table submenu

--- app/web/base/views.py
+++ app/web/base/views.py
@@ -1,23 +1,8 @@
 # -*- coding:utf-8 -*-
 from flask import render_template, url_for, flash, redirect
 from . import base
-from ..models import Aspect, Universum
 
 
 @base.route('/')
 def index():
-    universums = Universum.query.all()
-    first_row_of_universums = (universum for universum in universums if \
-                    universums.index(universum) % 2 == 0)
-    second_row_of_universums = (universum for universum in universums if \
-                    universums.index(universum) % 2 == 1)
-    superaspects = Aspect.query.filter_by(superaspect_id=None)
-    if len(list(superaspects)) == 2:
-        (x_aspects, y_aspects) = (superaspect.subaspects for superaspect in superaspects)
-        return render_template('base/index.html',
-                               x_aspects=reversed(x_aspects),
-                               y_aspects=y_aspects,
-                               first_row_of_universums=reversed(list(first_row_of_universums)),
-                               second_row_of_universums=reversed(list(second_row_of_universums)))
-    else:
-        return render_template('base/index.html')
+    return redirect(url_for('tables.get'))
--- app/web/tables/views.py
+++ app/web/tables/views.py
@@ -2,7 +2,7 @@
 from flask import render_template, url_for, flash, redirect
 from . import tables
 from ... import db
-from ..models import Table
+from ..models import Table, Aspect
 from .forms import TableForm
 
 
@@ -14,7 +14,22 @@ def get(table_id=None):
         return render_template('tables/get.html', tables=tables)
     else:
         table = Table.query.get_or_404(table_id)
-        return render_template('tables/get_by_id.html', table=table)
+        universums = list(table.universums)
+        first_row_of_universums = (universum for universum in universums if \
+                                   universums.index(universum) % 2 == 0)
+        second_row_of_universums = (universum for universum in universums if \
+                                    universums.index(universum) % 2 == 1)
+        superaspects = Aspect.query.filter_by(superaspect_id=None)
+        if len(list(superaspects)) == 2:
+            (x_aspects, y_aspects) = (superaspect.subaspects for superaspect in superaspects)
+            return render_template('tables/get_by_id.html',
+                                   table=table,
+                                   x_aspects=reversed(x_aspects),
+                                   y_aspects=y_aspects,
+                                   first_row_of_universums=reversed(list(first_row_of_universums)),
+                                   second_row_of_universums=reversed(list(second_row_of_universums)))
+        else:
+            return render_template('tables/get_by_id.html', table=table)
 
 @tables.route('/tables/create', methods=['GET', 'POST'])
 def create():
@@ -24,7 +39,7 @@ def create():
         db.session.add(table)
         db.session.commit()
         flash('Таблица была создана')
-        table = Table.query.filter_by(name=table.name).first()
+        table = Table.query.get_or_404(table.id)
         return redirect(url_for('.get', table_id=table.id))
     return render_template('tables/create.html', form=form)
 
